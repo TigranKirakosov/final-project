ec6712a16b4bdaace98582f7835865e0dc246e38
Tigran Kirakosov <tigran_kirakosov@epam.com>
2019-12-12 00:03:23 +0300

    Updates

--- src/fetchapi/restful-api.links.js
+++
@@ -1,7 +0,0 @@
-const restApiLinks = {
-  DB: 'http://localhost:3000/db/',
-  freePokemons: 'http://localhost:3000/freePokemons',
-  capturedPokemons: 'http://localhost:3000/capturedPokemons'
-};
-
-export default restApiLinks;
--- src/utils/utilities.js
+++
@@ -1,2 +0,0 @@
-export const capitalizeWord = word =>
-  word.split('')[0].toUpperCase() + word.slice(1);
